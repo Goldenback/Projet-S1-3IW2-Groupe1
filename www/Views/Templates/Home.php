a45798147b33db322f2553bfe53139c647431b77
Toma Chang <toma11chang@gmail.com>
2024-01-19 13:56:43 +0100

    fix : Docker-compose and nginx.conf

--- www/Views/Templates/Home.php
+++ www/Views/Templates/Home.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="">
 </head>
 <body>
 
-<div class="container">
+<div class="">
     <h1>Dashboard</h1>
-    <a href="/config" class="btn">Créer une page</a>
-    <a href="/logout" class="btn">Se déconnecter</a>
+    <a href="/config">Créer une nouvelle page</a>
+    <a href="/logout">Se déconnecter</a>
 </div>
 
 </body>
