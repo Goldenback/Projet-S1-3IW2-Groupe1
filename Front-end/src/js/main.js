35233d58373f144f434a394f8d94c475f7e8578f
Toma Chang <toma11chang@gmail.com>
2024-01-26 14:54:13 +0100

    feat : Ajout des fichiers du framework

--- Front-end/src/js/main.js
+++ Front-end/src/js/main.js
@@ -1,21 +1,3 @@
-import '../../dist/css/style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import "../css/main.scss";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import "./components/navbar.js";
