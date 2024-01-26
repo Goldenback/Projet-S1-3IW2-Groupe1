4dfdb508fb8be7ebcfc2cef0e9566d83f44177c2
Toma Chang <toma11chang@gmail.com>
2024-01-26 15:28:30 +0100

    fix : mise à jour fichier fonts

--- Front-end/dist/js/main.js
+++ Front-end/dist/js/main.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", () => {
-    document.querySelectorAll(".navbar_toggle_button").forEach((e) => {
-        const a = document.createElement("span");
-        e.append(a), e.onclick = () => {
-            const o = e.getAttribute("data-target"), t = document.querySelector(o);
-            t.classList.toggle("toggled"), e.classList.toggle("toggled"), t.classList.contains("toggled") ? t.style.maxHeight = t.scrollHeight + "px" : t.style.maxHeight = 0;
-        };
-    });
+  document.querySelectorAll(".navbar_toggle_button").forEach((e) => {
+    const a = document.createElement("span");
+    e.append(a), e.onclick = () => {
+      const o = e.getAttribute("data-target"), t = document.querySelector(o);
+      t.classList.toggle("toggled"), e.classList.toggle("toggled"), t.classList.contains("toggled") ? t.style.maxHeight = t.scrollHeight + "px" : t.style.maxHeight = 0;
+    };
+  });
 });
