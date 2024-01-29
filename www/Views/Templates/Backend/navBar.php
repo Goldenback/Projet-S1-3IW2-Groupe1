582b48d4385203f15603707b614987384bf72d01
Toma Chang <toma11chang@gmail.com>
2024-01-29 16:36:49 +0100

    fix : Navbar terminé + début page Mes informations

--- www/Views/Templates/Backend/navBar.php
+++ www/Views/Templates/Backend/navBar.php
@@ -2,34 +2,61 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
+    <title>Back-Office</title>
     <link rel="stylesheet" href="../../../Front-end/Workspace/dist/css/main.css">
 </head>
-<body id="body-pd">
+
+<body id="body">
 
 <nav class="l-navbar">
-    <div class="menu-section">
-        <a>Menu</a>
-    </div>
-    <div class="navbar_toggle"><span class="menu-icon">&#9776;</span></div>
-
-    <br>
-
-    <div>
-        <a href="/Config" class="nav_logo">Dashboard &#128200;</a>
+    <div id="menu-toggle" class="navbar_toggle">
+        <span class="menu-icon">&#9776;</span>
     </div>
 
-    <a href="/Config" class="nav_logo">Templating &#9881;</a>
+    <div class="nav-item">
+        <a href="/Config" class="nav-link">
+            <span class="nav-icon">&#128200;</span>
+            <span class="nav-text">Dashboard</span>
+        </a>
+    </div>
 
-    <a href="/Config" class="nav_logo">Commentaire &#9998;</a>
+    <div class="nav-item">
+        <a href="templates" class="nav-link">
+            <span class="nav-icon">&#128194;</span>
+            <span class="nav-text">Templating</span>
+        </a>
+    </div>
 
-    <a href="/usermanagement" class="nav_logo">Mes Informations &#128101;</a>
+    <div class="nav-item">
+        <a href="/comments" class="nav-link">
+            <span class="nav-icon">&#128203;</span>
+            <span class="nav-text">Commentaire</span>
+        </a>
+    </div>
 
-    <a href="/usermanagement" class="nav_logo">Pages &#128196;</a>
+    <div class="nav-item">
+        <a href="/usermanagement" class="nav-link">
+            <span class="nav-icon">&#128209;</span>
+            <span class="nav-text">Mes Informations</span>
+        </a>
+    </div>
 
-    <div class="logout-section">
-        <a href="/logout">Se déconnecter</a>
+    <div class="nav-item">
+        <a href="/mypage" target="blank" class="nav-link">
+            <span class="nav-icon">&#128221;</span>
+            <span class="nav-text">Pages</span>
+        </a>
+    </div>
+
+    <div class="logout-section nav-item">
+        <hr>
+        <a href="/logout" class="nav-link">
+            <span class="nav-icon">&#128244;</span>
+            <span class="nav-text">Se déconnecter</span>
+        </a>
     </div>
 </nav>
+
 
 <script>
     document.addEventListener('DOMContentLoaded', function () {
