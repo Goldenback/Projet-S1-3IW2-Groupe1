68e9f9826c1db08341a88f4823c14fd851aaeb88
CHANG-Toma <toma11chang@gmail.com>
2024-02-09 16:56:23 +0100

    fix : contact data form + flexbox

--- www/Views/Templates/Frontend/layout/contact-data.php
+++ www/Views/Templates/Frontend/layout/contact-data.php
@@ -1,10 +1,10 @@
 <section class="contact-menu">
     <div class="row">
-
+        
         <div class="content">
             <div class="icon-envelope">
                 <div class="icon">
-                    <img src="../../../Front-end/Workspace/assets/icon-svg/email.svg">
+                    <img src="../../../Front-end/Workspace/assets/svg/icon/email.svg">
                 </div>
             </div>
             <div class="contact-info">
@@ -12,13 +12,15 @@
                     <div class="heading">Email</div>
                     <div class="text">Feel free to reach out to us via email for any inquiries or support needs.</div>
                 </div>
-                <div class="link">example@mymail.com</div>
+                <div class="link">contact@simplify.com</div>
             </div>
         </div>
 
         <div class="content">
-            <div class="icon">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/phone.svg">
+            <div class="icon-phone">
+                <div class="icon">
+                    <img src="../../../Front-end/Workspace/assets/svg/icon/phone.svg">
+                </div>
             </div>
             <div class="contact-info">
                 <div class="content">
@@ -30,15 +32,17 @@
         </div>
 
         <div class="content">
-            <div class="icon">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/pin-map.svg">
+            <div class="icon-map">
+                <div class="icon">
+                    <img src="../../../Front-end/Workspace/assets/svg/icon/pin-map.svg">
+                </div>
             </div>
             <div class="contact-info">
                 <div class="content">
                     <div class="heading">Office</div>
                     <div class="text">Visit our office for a face-to-face consultation by appointment.</div>
                 </div>
-                <div class="link">123 Business Ave, Metropolis, XY 45678</div>
+                <div class="link">18 Rue des étudiants, 12345 France</div>
             </div>
         </div>
 
