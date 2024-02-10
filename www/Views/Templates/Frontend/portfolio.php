08dd151ea7ee846e47db76a2ac835309c8e4f71b
CHANG-Toma <toma11chang@gmail.com>
2024-02-11 00:49:09 +0100

    fix : page portfolio + img

--- www/Views/Templates/Frontend/portfolio.php
+++ www/Views/Templates/Frontend/portfolio.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Simplify</title>
+    <title>Portfolio</title>
     <link rel="stylesheet" href="../../../Front-end/Workspace/dist/css/main.css"/>
 </head>
 
@@ -140,48 +140,46 @@
     <div class="Content">
         <div class="Column">
             <div class="Stars">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
             </div>
             <p class="Quote">Our platform has been a game-changer for my portfolio. It's easy to use and has helped me showcase my work effectively.</p>
             <div class="Avatar">
-                <img src="https://via.placeholder.com/56x56" alt="John Doe" class="AvatarImage">
+                <img src="../../../Front-end/Workspace/assets/img/page-portfolio/portrait-1.png" alt="John Doe" class="AvatarImage">
                 <div class="AvatarContent">
                     <div class="Name">John Doe</div>
                     <div class="Role">Photographer, ABC Studio</div>
                 </div>
                 <div class="Divider"></div>
                 <div class="CompanyLogo WebflowBlack">
-                    <img src="../../../Front-end/Workspace/assets/icon-svg/webflow-svgrepo-com.svg">
-
+                    <img src="../../../Front-end/Workspace/assets/svg/brand/webflow-2.svg">
                 </div>
             </div>
 
         </div>
         <div class="Column">
             <div class="Stars">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
-                <img src="../../../Front-end/Workspace/assets/icon-svg/stars-svgrepo-com.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg">
+                <img src="../../../Front-end/Workspace/assets/svg/icon/star.svg"> 
             </div>
             <p class="Quote">
                 I highly recommend this platform to all artists looking to manage their portfolios. It has streamlined my workflow and boosted my online presence.
             </p>
             <div class="Avatar">
-                <img src="https://via.placeholder.com/56x56" alt="Jane Smith" class="AvatarImage">
+                <img src="../../../Front-end/Workspace/assets/img/page-portfolio/portrait-2.png" alt="Jane Smith" class="AvatarImage">
                 <div class="AvatarContent">
                     <div class="Name">Jane Smith</div>
                     <div class="Role">Musician, XYZ Records</div>
                 </div>
                 <div class="Divider"></div>
                 <div class="CompanyLogo WebflowBlack">
-                    <img src="../../../Front-end/Workspace/assets/icon-svg/webflow-svgrepo-com.svg">
-
+                    <img src="../../../Front-end/Workspace/assets/svg/brand/webflow-2.svg">
                 </div>
             </div>
         </div>
