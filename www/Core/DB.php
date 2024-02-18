d2f1f85f4599504b929b443af9412900958a725f
Goldenback <iliesrimani.work@gmail.com>
2024-02-18 23:41:01 +0100

    Updated the connection with the database

--- www/Core/DB.php
+++ www/Core/DB.php
@@ -4,33 +4,43 @@ namespace App\Core;
 
 use PDO;
 
-/**
- * @method prepare(string $string)
- */
 class DB
 {
+    private static ?DB $instance = null;
+    private PDO $connection;
 
-    public function __construct()
+    private string $host = 'postgres';
+    private string $database = 'db_name';
+    private string $user = 'root';
+    private string $password = 'root';
+
+    private function __construct()
     {
-    }
+        $dsn = "pgsql:host=$this->host;dbname=$this->database";
 
-    function getDatabaseConnection(): PDO
-    {
-        $host = 'db';
-        $db = 'db_name';
-        $user = 'super_user_name';
-        $pass = 'smth_smth';
-
-        $dsn = "pgsql:host=$host;dbname=$db";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
 
         try {
-            return new PDO($dsn, $user, $pass, $options);
+            $this->connection = new PDO($dsn, $this->user, $this->password, $options);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
     }
 
+    public static function getInstance(): ?DB
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
 }
